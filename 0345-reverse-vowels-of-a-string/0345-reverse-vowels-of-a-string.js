2b3c1cd91d65e2c63d8bff449aa23be7160188e3
Claudia Serrano <perezserranoclaudia@gmail.com>
2026-07-28 20:49:29 +0200

    Time: 6 ms (72.67%), Space: 60.3 MB (56.8%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.js
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.js
@@ -3,43 +3,20 @@
  * @return {string}
  */
 var reverseVowels = function(s) {
-    let vocals = [];
-    for (let i = 0; i < s.length; i++) {
-        if (s[i] == ("A")
-            || s[i] == ("E") 
-            || s[i] == ("I")
-            || s[i] == ("O")
-            || s[i] == ("U")
-            || s[i] == ("a") 
-            || s[i] == ("e")
-            || s[i] == ("i")
-            || s[i] == ("o")
-            || s[i] == ("u")
-            ) { 
-                vocals.push(s[i]);
-            }
+    const vowels = "aeiouAEIOU";
+    const chars = s.split("");
+
+    let l = 0, r = chars.length - 1;
+
+    while(l < r) {
+        while(l < r && !vowels.includes(chars[l])) l++;
+
+        while(l < r && !vowels.includes(chars[r])) r--;
+
+        [chars[l], chars[r]] = [chars[r], chars[l]];
+        l++;
+        r--;
     }
 
-    let newS = []
-
-    for (let i = 0; i < s.length; i++) {
-        if (s[i] == ("A")
-            || s[i] == ("E") 
-            || s[i] == ("I")
-            || s[i] == ("O")
-            || s[i] == ("U")
-            || s[i] == ("a") 
-            || s[i] == ("e")
-            || s[i] == ("i")
-            || s[i] == ("o")
-            || s[i] == ("u")
-            ) { 
-                newS[i] = vocals.pop();
-                continue;
-            }
-
-        newS[i] = s[i];
-    }
-
-    return newS.join("");
+    return chars.join("");
 };
