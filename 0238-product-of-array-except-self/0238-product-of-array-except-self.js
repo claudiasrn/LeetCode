880084dfa6f5810e082fd1d91d875dc31bbbeb1d
Claudia Serrano <perezserranoclaudia@gmail.com>
2026-07-29 20:25:53 +0200

    Time: 16 ms (26.44%), Space: 70.7 MB (57.97%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.js
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.js
@@ -3,19 +3,20 @@
  * @return {number[]}
  */
 var productExceptSelf = function(nums) {
+    const n = nums.length;
     let leftProducts = new Array(nums.length).fill(1);
     let rightProducts = new Array(nums.length).fill(1);
     let result = new Array(nums.length).fill(0);
 
-    for (let i = 1; i < nums.length; i++) {
+    for (let i = 1; i < n; i++) {
         leftProducts[i] = leftProducts[i - 1] * nums[i - 1];
     }
 
-    for (let i = nums.length - 2; i >= 0; i--) {
+    for (let i = n - 2; i >= 0; i--) {
         rightProducts[i] = rightProducts[i + 1] * nums[i + 1];
     }
 
-    for (let i = 0; i < nums.length; i++) {
+    for (let i = 0; i < n; i++) {
         result[i] = leftProducts[i] * rightProducts[i];
     }
 
